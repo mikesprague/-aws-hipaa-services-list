d8cb31be0ace9a742c87fc2c9e40ad09357c69fb
Michael Sprague <ms388@cornell.edu>
2022-05-31 21:29:11 -0400

    :recycle: Skip updates when no changes to source page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import * as core from '@actions/core';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import fs from 'fs';
+import { createRequire } from 'module';
 
 const { hrtime } = process;
+
+// create shim for 'require' (not natively available in esm style js)
+const require = createRequire(import.meta.url);
 
 (async () => {
   const debugStart = hrtime();
@@ -18,6 +23,10 @@ const { hrtime } = process;
     outputDir: 'docs/',
     filename: 'hipaa-services-list',
   };
+
+  // read in previous results to get date for comparison
+  const lastResults = require('../docs/hipaa-services-list.json');
+  const { lastUpdated: previousLastUpdated } = lastResults;
 
   // make http call to fetch html
   const markup = await axios
@@ -45,37 +54,50 @@ const { hrtime } = process;
   // set lastUpdated in results object
   hipaaServices.lastUpdated = lastUpdatedString;
 
-  // loop over services list and add to array in results object
-  $(config.selectors.services).each((i, elem) => {
-    const serviceName = $(elem).text().trim();
-    hipaaServices.serviceList.push(serviceName);
-  });
+  // set var with date string comparison results
+  const hasUpdates =
+    previousLastUpdated.toLowerCase() !== lastUpdatedString.toLowerCase();
 
-  // create JSON string from object data to write to file
-  const jsonDataAsString = `${JSON.stringify(hipaaServices, null, 2)}\n`;
+  // if date strings are different, proceed with update
+  if (hasUpdates) {
+    // loop over services list and add to array in results object
+    $(config.selectors.services).each((i, elem) => {
+      const serviceName = $(elem).text().trim();
+      hipaaServices.serviceList.push(serviceName);
+    });
 
-  // open html list
-  const htmlPrefix = '<ul>\n';
-  // build html list from data
-  const htmlBody = hipaaServices.serviceList.map(
-    (serviceName) => `  <li>${serviceName}</li>\n`,
-  );
-  // close html list and add last updated string
-  const htmlSuffix = `</ul>
+    // create JSON string from object data to write to file
+    const jsonDataAsString = `${JSON.stringify(hipaaServices, null, 2)}\n`;
+
+    // open html list
+    const htmlPrefix = '<ul>\n';
+    // build html list from data
+    const htmlBody = hipaaServices.serviceList.map(
+      (serviceName) => `  <li>${serviceName}</li>\n`,
+    );
+    // close html list and add last updated string
+    const htmlSuffix = `</ul>
 <p><small><em>HIPAA services list last updated by AWS on ${lastUpdatedString}</em></small></p>
 `;
 
-  // build full html string to write to file
-  const htmlToOutput = `${htmlPrefix}${htmlBody.join('')}${htmlSuffix}`;
+    // build full html string to write to file
+    const htmlToOutput = `${htmlPrefix}${htmlBody.join('')}${htmlSuffix}`;
 
-  // write json file
-  const jsonFileName = `${config.outputDir}${config.filename}.json`;
-  await fs.writeFileSync(jsonFileName, jsonDataAsString);
-  console.log(`JSON file written: ${jsonFileName}`);
-  // write html file
-  const htmlFileName = `${config.outputDir}${config.filename}.html`;
-  await fs.writeFileSync(htmlFileName, htmlToOutput);
-  console.log(`HTML file written: ${htmlFileName}`);
+    // write json file
+    const jsonFileName = `${config.outputDir}${config.filename}.json`;
+    await fs.writeFileSync(jsonFileName, jsonDataAsString);
+    console.log(`JSON file written: ${jsonFileName}`);
+
+    // write html file
+    const htmlFileName = `${config.outputDir}${config.filename}.html`;
+    await fs.writeFileSync(htmlFileName, htmlToOutput);
+    console.log(`HTML file written: ${htmlFileName}`);
+  } else {
+    // output to run logs
+    console.log('No updates to services page, nothing to process');
+  }
+  // set env var that workflow can use for conditional processing
+  core.exportVariable('HAS_UPDATES', true);
 
   // output execution time
   const debugEnd = hrtime(debugStart);
